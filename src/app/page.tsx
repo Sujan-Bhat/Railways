e278327743ed6acb060f533ac32c4511552334ec
Sharan U <sharan85444@gmail.com>
2025-05-21 18:31:35 +0530

    footeradded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./layout/header";
 import Hero from "./layout/hero";
+import Footer from "./layout/footer";
+
 
 export default function Home() {
   return (
     <>
       <Header/>
       <Hero/>
+      <Footer/>
     </>
   );
 }
